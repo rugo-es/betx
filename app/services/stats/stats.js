c730559df49b73d6210a77e390ce5ff732f0f65c
rgonzalez <rgonzalez@mkdautomotive.com>
2021-05-23 18:27:11 +0200

    stats v1.0

---
+++ app/services/stats/stats.js
@@ -0,0 +1,75 @@
+"use strict"
+
+const { Op } = require("sequelize");
+const models = require('../../models')
+const Team = models.Team
+const Match = models.Match
+const Stats = models.Stats
+
+
+const getTeams = () => {
+  return new Promise((resolve, reject) => {
+    Team.findAll()
+    .then(data => {
+      resolve(data)
+    })
+    .catch(err => {
+      reject(err)
+    })
+  })
+}
+
+const getMatchesByTeam = (team) => {
+  return new Promise((resolve, reject) => {
+    Match.findAll({where: { [Op.or]: [{localId: team}, {visitorId: team}]}, include: ['local', 'visitor'] })
+    .then(data => {
+      resolve(data)
+    })
+    .catch(err => {
+      reject(err)
+    })
+  })
+}
+
+const addStats = (stats) => {
+  return new Promise((resolve, reject) => {
+    Stats.create(stats).then((stats) => {
+      resolve(stats)
+    }).catch(err => {
+      reject(err)
+    })
+  })
+}
+
+async function run(){
+
+  var teams = await getTeams()
+  for (let j = 0; j < teams.length; j++){
+    let team = teams[j].id
+    var matches = await getMatchesByTeam(team)
+    let num_matches = 0, num_matches_finished = 0, wins = 0, losses = 0, ties = 0
+    for (let i = 0; i < matches.length; i++){
+      num_matches++
+      if(matches[i].state == 'Finalizado'){ num_matches_finished++ }
+      if( (team == matches[i].localId && matches[i].result == '1') || (team == matches[i].visitorId && matches[i].result == '2') ){ wins++ }
+      if( (team == matches[i].localId && matches[i].result == '2') || (team == matches[i].visitorId && matches[i].result == '1') ){ losses++ }
+      if(matches[i].result == 'X'){ ties++ }
+    }
+    let stats = {
+      teamId: team,
+      num_matches: num_matches,
+      num_matches_finished: num_matches_finished,
+      wins: wins,
+      per_wins: (wins/num_matches_finished),
+      losses: losses,
+      per_losses: (losses/num_matches_finished),
+      ties: ties,
+      per_ties: (ties/num_matches_finished)
+    }
+    console.log(stats)
+    await addStats(stats)
+  }
+  
+}
+
+run()
